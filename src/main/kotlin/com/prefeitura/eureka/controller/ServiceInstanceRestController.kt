53ba1158dc3196a675c1e570cb3581babaa8b186
Patrick Bathu <patrick.bathu@compasso.com.br>
2021-04-07 07:32:41 -0300

    Ajustes

--- src/main/kotlin/com/prefeitura/eureka/controller/ServiceInstanceRestController.kt
+++ src/main/kotlin/com/prefeitura/eureka/controller/ServiceInstanceRestController.kt
@@ -10,7 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cloud.client.discovery.DiscoveryClient
+import org.springframework.web.bind.annotation.CrossOrigin
 
+@CrossOrigin(origins = arrayOf("*"))
 @RestController
 class ServiceInstanceRestController {
 
